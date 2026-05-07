60464b73426f464337d05ba5b6c683420f9a70ad
sagetobyn <sagetobyn@gmail.com>
2026-05-07 16:29:13 +0530

    disable ts lint blocking

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
   experimental: {
     cpus: 1,
     workerThreads: false,
     webpackBuildWorker: false,
-    staticGenerationMinPagesPerWorker: 1000
-  }
+    staticGenerationMinPagesPerWorker: 1000,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
