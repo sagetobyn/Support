7ff71a3de2740a4f0239fd24554425a5db51a15b
sagetobyn <sagetobyn@gmail.com>
2026-05-12 06:52:03 +0530

    improvement

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "next-env.d.ts",
+      "next-build.log",
+      "*.png",
+      "*.xlsx",
+      "Research/**",
+    ],
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "react/no-unescaped-entities": "off",
+    },
+  },
+];
